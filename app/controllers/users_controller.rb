28674a3891d5461cb4115120493a6aef2fd277d4
Rodolfo Mata <rodmatrod@gmail.com>
2020-06-09 21:18:20 -0500

    Add html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
+  include UsersHelper
   def new
-
+    @user = User.new
   end
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def user_params
+    params.require(:user).permit(:authenticity_token, :username, :email, :password)
+  end
 end
